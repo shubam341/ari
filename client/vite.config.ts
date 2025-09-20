ce08973d5bbdc416a84ab55f5580668cd59dd86d
shubam <chshivam148@gmail.com>
2025-09-21 03:46:17 +0530

    Fix Vite base path and Vercel config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // <-- important for Vite build to work on Vercel
   server: {
     host: "::",
     port: 8080,
